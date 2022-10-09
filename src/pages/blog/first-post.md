051f396bba596ab72ac7826b920f4c02fd1fc548
TheGoodBoi <53830363+TheGoodBoi411@users.noreply.github.com>
2022-10-09 13:56:26 -0700

    First files

---
+++ src/pages/blog/first-post.md
@@ -0,0 +1,44 @@
+---
+layout: ../../layouts/Blog.astro
+
+title: "First Post"
+date: "October 1, 2022"
+desc: "I made a website! Why?"
+id: "first-post"
+---
+
+<div class="text-center" style="width:23%">
+<p class="mb-2"><b>And we are live!</b></p>
+</div>
+
+<p class="mb-1">
+<img 
+src="https://cdn.discordapp.com/attachments/803471147898241027/1026025063515951114/Iamlive.png"
+alt="tay zonday said this" 
+width="23%" 
+class="rounded-bottom mb-0">
+</p>
+
+> Tay Zonday
+
+# Website
+
+Give a man a fish, he eats for a day.
+Give a man a playlist of HTML tutorials,  he- huh?
+
+## Idea
+
+A while ago I saw a YouTube Short about having your own website.
+I kept it in my mind, until I saw a [video](https://youtu.be/BZqzhmlTkAc) about someone's favorite personal websites.
+These unique websites - mostly the first one - inspired me to make my own.
+I started by making a landing page and the Beat Saber graphic you can see [here](../beat-saber).
+Then I gave up.
+Turns out making a very nice website as your first is not easy.
+So, I moved on.
+
+But, as I was randomly looking at Discord profiles I saw https://kivalevan.me.
+Someone who also plays Beat Saber has a cool little website that isn't too complex?
+I convinced myself that I should make one.
+
+So I started with [Astro](https://astro.build) (idea taken from Kival) and [Bootstrap](https://getbootstrap.com), ready for something much better.
+I made a simple(r) landing page, and kept expanding until I was happy!
---
+++ src/scripts/bs.js
@@ -0,0 +1,75 @@
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// console.log("ODNF");
+
+// let Btop = Math.round(randomInt(0,2)/3 * 1000) / 10;
+// let Bleft = randomInt(0,3)/4*100
+// Btop += "%"
+// Bleft += "%"
+// document.getElementById("hitB").style.top = Btop;                   
+// document.getElementById("hitB").style.left = Bleft;
+
+// let Rtop = Math.round(randomInt(-1,1)/3 * 1000) / 10;
+// let Rleft = randomInt(0,3)/4*100
+// Rtop += "%"
+// Rleft += "%"
+// document.getElementById("hitR").style.top = Rtop;                   
+// document.getElementById("hitR").style.left = Rleft;
+
+async function blueNote() {
+
+    const blueNote = document.createElement("div");
+    blueNote.id = "noteB";
+    blueNote.style = `transform: translate(${randomInt(0,3)}00%, ${randomInt(0,2)}00%);`
+    blueNote.onclick = blueGone;
+    document.getElementById('beat-saber').appendChild(blueNote);
+
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    blueNote.remove();
+
+}
+
+async function redNote() {
+
+    const redNote = document.createElement("div");
+    redNote.id = "noteR";
+    redNote.style = `transform: translate(${randomInt(0,3)}00%, ${randomInt(0,2)}00%);`
+    redNote.onclick = redGone;
+    document.getElementById('beat-saber').appendChild(redNote);
+
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    redNote.remove();
+
+}
+
+async function noteLoop () {
+    blueNote();
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    redNote();
+}
+
+setInterval(noteLoop, 2000);
+
+async function blueGone() {
+    noteCLick("noteB");
+}
+async function redGone() {
+    noteCLick("noteR");
+}
+async function noteCLick(id) {
+    let note = document.getElementById(id);
+    note.animate([
+        { opacity: 1, margin: '0px' },
+        { opacity: 0, margin: '-150px' }
+    ], {
+        duration: 150,
+        iterations: 1
+    });
+    await new Promise(resolve => setTimeout(resolve, 100));
+    note.remove();
+}
