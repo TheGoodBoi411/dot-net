c126a433e55b92733dbc92e82f6e538810550bf8
TheGoodBoi411 <andrewgoodman411@gmail.com>
2022-10-09 18:34:39 -0700

    Fix links again

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://TheGoodBoi411.github.io',
-    base: '/dot-com',
+    base: '/dot-net',
 });
